ee86e2c1a87f37cf4fb5fd57beb808be95195d31
paranoia1906 <anthonymledesma@gmail.com>
2018-02-04 21:01:46 +0000

    Comment, beautify, and simplify code for submission to Git and Edx.

--- FontChooser.js
+++ FontChooser.js
@@ -1,134 +1,101 @@
 class FontChooser extends React.Component {
     constructor(props) {
 		super(props);
-		var checked = this.props.bold;
-		var minimum = parseInt(this.props.min) > 1 ? parseInt(this.props.min) : 1 ;
-		var maximum = parseInt(this.props.max);
-		if (maximum < minimum) [minimum, maximum] = [maximum, minimum];
-		var sizeProp = parseInt(this.props.size);
-		var defaultSize = parseInt(this.props.size);
-		var shouldBeBold = this.props.bold;
-		if (sizeProp < minimum) {sizeProp = minimum;} else if (sizeProp > maximum) {sizeProp = maximum;}
+		var minimum = parseInt(this.props.min) > 1 ? parseInt(this.props.min) : 1 ; //Setting minimum text size.
+		var maximum = parseInt(this.props.max); //setting maximum text size. 
+		if (maximum < minimum) [minimum, maximum] = [maximum, minimum]; //If max is less than minimum, swap them.
 		this.state = {
-			hidden:true, 
-			size:parseInt(sizeProp),
-			textMin:minimum,
+			hidden:true, //Acts as control for all hidden elements in application.
+			size:parseInt(this.props.size), //using parseInt to make sure we are not getting a string like '16'
+			textMin:minimum, //Setting calculated min and max to states.
 			textMax:maximum,
-			initialSize:defaultSize,
-			isChecked:checked,
-			bold:shouldBeBold == 'true'
+			isChecked:this.props.bold, //Checkbox should match status of props.bold
+			bold:this.props.bold == 'true' //Settings bold state to equal the initial bold property setting.
 		};
     }
 	
 	handleClick() {
-		this.setState( { hidden : !this.state.hidden } );		
+		this.setState( { hidden : !this.state.hidden } );
+		//Handle click will alternate boolean value for hidden.		
 	}
 		
 
 	handleOnChange() {
-		if (this.state.bold == true) {
-      this.setState({bold: false});}
-    else{
-      this.setState({bold: true});}
-
+		if (this.state.bold == true) { //Will ensure that bold status is swapped at the same time checkbox is changed.
+			this.setState({bold: false});
+		} else {
+			this.setState({bold: true});
+		}
 	}
 
 	handlePlusClick() {
-		if (this.state.size < this.state.textMax){
-			var state = parseInt(this.state.size) + 1;
+		if (this.state.size < this.state.textMax){ //Checking that we are not at max already.
+			var state = parseInt(this.state.size) + 1; //If not at max, increment size state.
 			this.setState( {size: parseInt(state)});
 		}
 	}
 
 	handleMinusClick() {
-		if (this.state.size > this.state.textMin){
-			var state = parseInt(this.state.size) - 1;
+		if (this.state.size > this.state.textMin){ //Checking that we are not at min already.
+			var state = parseInt(this.state.size) - 1; //If not at min, increment size state.
 			this.setState( {size: parseInt(state)});
 		}
 	}
 
-	handleDoubleClick() {
-		var revert = this.state.initialSize;
-		this.setState({size:parseInt(revert)});
-
+	handleDoubleClick() { //Only triggers when double clicking on font size span.
+		var revert = this.props.size; //revert is a temp holder of props.size. 
+		this.setState({size:parseInt(revert)}); //Using revert and parseInt to ensure actual number instead of '16'
 	}
 
-	renderTextColor() {
+	renderTextColor() { //If either at max or min already then set color to red. 
 		if (this.state.size == this.state.textMax || this.state.size == this.state.textMin ) {
 			var fontColor = {color : 'red'}
-		 } else {
+		 } else { // if not at the max, set color of text to black.
 			var fontColor = {color : 'black'}
 		 }
-		return fontColor;
+		return fontColor; //Return style to be used with the font size text.
 	}
-
-	// renderBoldText() {
-	// 	if (this.state.bold == 'true') {
-	// 		var fontWeight = 'bold';
-	// 	} 
-	// 	if (this.state.bold == 'false') {
-	// 		var fontWeight = 'normal';
-	// 	}
-	// 	return fontWeight;
-		
-	// }
-
-	
  
     render() {
-		var weight;
-		if (this.state.bold == true) {weight = 'bold'} else {weight = 'normal'}
-
-		var renderCheck;
-		if (weight == 'bold') {renderCheck = true;} else {renderCheck = false;}
-		
-		var curSize = parseInt(this.state.size);
-		
-		var textStyle = {
-			fontWeight: weight,
-			fontSize: parseInt(curSize)
+		var weight; //bold or not
+		var renderCheck; //checkbox to match bold or not.
+		var curSize = parseInt(this.state.size); //Take current size state and set to integer.
+		var fColor = this.renderTextColor(); //Run check against max or min for red text.
+		if (this.state.bold == true) {weight = 'bold'} else {weight = 'normal'} //Are we bold or normal.
+		if (weight == 'bold') {renderCheck = true;} else {renderCheck = false;} //Make sure we match bold satus.
+		var textStyle = { //Var structure this way to mimic style html tags.
+			fontWeight: weight, 
+			fontSize: parseInt(curSize) 
 		}
-		
-		
-		var fColor = this.renderTextColor();
-		console.log(renderCheck);
 		return(
 		   <div>
-
-		   <input 
-		   type="checkbox" 
-		   id="boldCheckbox" 
+		   <input type="checkbox" id="boldCheckbox" 
 		   hidden={this.state.hidden} 
 		   defaultChecked={renderCheck}
 		   checked={renderCheck}
 		   onChange={this.handleOnChange.bind(this)} 
 			/>
 
-		   <button 
-		   id="decreaseButton" 
+		   <button id="decreaseButton" 
 		   hidden={this.state.hidden} 
 		   onClick={this.handleMinusClick.bind(this)} >-</button>
 
-		   <span 
-		   id="fontSizeSpan" 
+		   <span id="fontSizeSpan" 
 		   hidden={this.state.hidden} 
 		   style={fColor} 
 		   onDoubleClick={this.handleDoubleClick.bind(this)} >
 		   {this.state.size}
 		   </span>
 
-		   <button 
-		   id="increaseButton" 
+		   <button id="increaseButton" 
 		   hidden={this.state.hidden} 
 		   onClick={this.handlePlusClick.bind(this)} >+</button>
 		   
-		   <span 
-		   id="textSpan" 
+		   <span id="textSpan" 
 		   onClick={this.handleClick.bind(this)} 
 		   style={textStyle}>
 		   {this.props.text}
 		   </span>
-		   
 	       </div> 
 		);
     }
--- README.md
+++ README.md
@@ -5,9 +5,14 @@ Once the click event ocurrs a checkbox, a font size text field, and two buttons 
 
 Requirements:
 1.) Button press should increment font size and string of text size appropreately. 
+
 2.)Button press should ignore when at max/min limitation for size.
+
 3.)Font size text should be red color when at max/min size.
+
 4.)Double click on font size text should reset to original setting.
+
 5.)Checkbox to always current bold status.
+
 
 Score = 100/100
